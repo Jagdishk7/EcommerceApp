817122f8b567f0c37e1556e446d9567834c3d758
Jagdeesh Kushwaha <pwdacait7@gmail.com>
2024-10-15 21:56:25 +0530

    ProductModel import fixed

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../models/orderModel.js";
-import Product from "../models/productModel.js";
+import Product from "../models/ProductModel.js";
 import { calcPrices } from "../utils/calcPrices.js";
 import { verifyPayPalPayment, checkIfNewTransaction } from "../utils/paypal.js";
 
--- server/seeder.js
+++ server/seeder.js
@@ -4,7 +4,7 @@ import colors from "colors";
 import users from "./data/users.js";
 import products from "./data/products.js";
 import User from "./models/userModel.js";
-import Product from "./models/productModel.js";
+import Product from "./models/ProductModel.js";
 import Order from "./models/orderModel.js";
 import connectDB from "./config/db.js";
 
